e96b23e81cd518da2fae6f134b1a7401a3675f2b
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2021-02-16 00:36:41 -0500

    thanks philll <3

--- A1/PG-260869949/pgsql/q05.sql
+++ A1/PG-260869949/pgsql/q05.sql
@@ -1,12 +1,13 @@
-SELECT *
+SELECT pname, pstartdate
 FROM project
 WHERE pstartdate > '2020-05-01' AND pstartdate < '2020-08-31'
+ORDER BY pstartdate, pname
 ;
 
 
---   pname   | pstartdate |  ptype   |  budget
--- ----------+------------+----------+----------
---  Otters   | 2020-05-13 | internal | 46100000
---  Kodiak   | 2020-05-20 | external | 50000000
---  Capybara | 2020-08-22 | external | 19500000
+--   pname   | pstartdate
+-- ----------+------------
+--  Otters   | 2020-05-13
+--  Kodiak   | 2020-05-20
+--  Capybara | 2020-08-22
 -- (3 rows)
