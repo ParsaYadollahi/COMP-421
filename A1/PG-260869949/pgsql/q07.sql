30ec3320ecc905dd868b5d41b9f338878120f79c
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2021-02-18 14:14:37 -0500

    DONE

--- A1/PG-260869949/pgsql/q07.sql
+++ A1/PG-260869949/pgsql/q07.sql
@@ -1,63 +1,29 @@
-SELECT t1.pname as "project name", t1.employeeid as "employee id"
+SELECT DISTINCT t1.pname, t1.employeeid
 FROM
 
 (SELECT t1.employeeid, t2.pname
-FROM documentauthors as t1 JOIN document as t2
+FROM documentauthors as t1 LEFT JOIN document as t2
 ON t1.documentid = t2.documentid)
 as t1
 
-JOIN
+LEFT JOIN
 
-(SELECT t1.employeeid, t1.pname
+(SELECT *
 FROM devassignments as t1)
  as t2
 
-ON t1.employeeid = t2.employeeid
+ON t1.employeeid = t2.employeeid and t1.pname = t2.pname
 
-WHERE
-  t1.employeeid = t2.employeeid
-  AND
-  t1.pname != t2.pname
+WHERE t2.asgndate IS NULL
 
-ORDER BY t1.pname
+ORDER BY t1.pname, t1.employeeid
 ;
 
-
---  project name | employee id
--- --------------+-------------
---  Albatrose    |       93752
---  Albatrose    |       82453
---  Albatrose    |       82453
---  Albatrose    |       82453
---  Albatrose    |       82453
---  Capybara     |       81532
---  Capybara     |       91243
---  Capybara     |       81532
---  Charles      |       82453
---  Charles      |       82453
---  Charles      |       82453
---  Charles      |       82453
---  Charles      |       81532
---  Charles      |       82102
---  Charles      |       82102
---  Charles      |       82102
---  Charles      |       87634
---  Charles      |       81532
---  Charles      |       81532
---  Kodiak       |       93401
---  Kodiak       |       82102
---  Kodiak       |       82102
---  Kodiak       |       93401
---  Kodiak       |       93401
---  Kodiak       |       82453
---  Kodiak       |       82453
---  Kodiak       |       90234
---  Kodiak       |       82102
---  Kodiak       |       93401
---  Otters       |       89374
---  Otters       |       93401
---  Otters       |       93401
---  Otters       |       93401
---  Snowy        |       93401
---  Snowy        |       93401
--- (35 rows)
+--   pname   | employeeid
+-- ----------+------------
+--  Capybara |      91243
+--  Charles  |      81532
+--  Kodiak   |      90234
+--  Kodiak   |      93401
+--  Snowy    |      93401
+-- (5 rows)
