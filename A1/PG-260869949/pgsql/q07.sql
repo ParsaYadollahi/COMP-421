1fef4100acf705f0e2f0cb98ae475a4a4ee1e494
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2021-02-18 16:38:27 -0500

    cleaned up q7

--- A1/PG-260869949/pgsql/q07.sql
+++ A1/PG-260869949/pgsql/q07.sql
@@ -1,21 +1,15 @@
 SELECT DISTINCT t1.pname, t1.employeeid
 FROM
-
-(SELECT t1.employeeid, t2.pname
-FROM documentauthors as t1 LEFT JOIN document as t2
-ON t1.documentid = t2.documentid)
-as t1
-
+  (
+    SELECT t1.employeeid, t2.pname
+    FROM documentauthors as t1 LEFT JOIN document as t2
+    ON t1.documentid = t2.documentid) as t1
 LEFT JOIN
-
-(SELECT *
-FROM devassignments as t1)
- as t2
-
+  (
+    SELECT *
+    FROM devassignments as t1) as t2
 ON t1.employeeid = t2.employeeid and t1.pname = t2.pname
-
 WHERE t2.asgndate IS NULL
-
 ORDER BY t1.pname, t1.employeeid
 ;
 
