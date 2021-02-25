750392cb350bd4eda11482c5157a2bb1f4b34289
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2021-02-24 20:56:01 -0500

    ALMOST DONE

--- P2/p2/createtbl.sql
+++ P2/p2/createtbl.sql
@@ -21,88 +21,98 @@ CREATE TABLE MYTEST01
 CREATE TABLE priority
 (
   prionum INTEGER NOT NULL,
-  cname VARCHAR(30) NOT NULL,
+  cname VARCHAR(255) NOT NULL,
   PRIMARY KEY (cname)
-);
-
-CREATE TABLE resident
-(
-	hinsurnum INTEGER NOT NULL,
-  name VARCHAR(30) NOT NULL,
-  gender VARCHAR(1) NOT NULL,
-  birthdate DATE NOT NULL,
-  phone VARCHAR(30) NOT NULL,
-  city VARCHAR(15) NOT NULL,
-  postalcode VARCHAR(15) NOT NULL,
-  streetaddr VARCHAR(30) NOT NULL,
-  regdate DATE NOT NULL,
-  cname VARCHAR(30) NOT NULL,
-  PRIMARY KEY (hinsurnum),
-  FOREIGN KEY (cname) REFERENCES priority
-);
-
-CREATE TABLE hospital
-(
-  lname VARCHAR(30) NOT NULL,
-  city VARCHAR(30) NOT NULL,
-  postalcode VARCHAR(30) NOT NULL,
-  address VARCHAR(30) NOT NULL,
-  PRIMARY KEY (lname)
-);
-
-CREATE TABLE nurse
-(
-  cnlnumber INTEGER NOT NULL,
-  nname VARCHAR(30) NOT NULL,
-  employer VARCHAR(30) NOT NULL,
-  PRIMARY KEY (cnlnumber),
-  FOREIGN KEY (employer) REFERENCES hospital
-);
-
-CREATE TABLE vacclocation
-(
-  lname VARCHAR(30) NOT NULL,
-  city VARCHAR(30) NOT NULL,
-  postalcode VARCHAR(30) NOT NULL,
-  address VARCHAR(30) NOT NULL,
-  PRIMARY KEY (lname)
-);
-
-CREATE TABLE vaccdates
-(
-  lname VARCHAR(30) NOT NULL,
-  vdate DATE NOT NULL,
-  PRIMARY KEY(lname, vdate),
-  FOREIGN KEY (lname) references vacclocation
 );
 
 CREATE TABLE vaccine
 (
-  vname VARCHAR(30) NOT NULL,
+  vname VARCHAR(255) NOT NULL,
   waitperiod INTEGER NOT NULL,
   doses INTEGER NOT NULL,
   PRIMARY KEY (vname)
 );
 
+CREATE TABLE resident
+(
+	hinsurnum INTEGER NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  gender VARCHAR(255) NOT NULL,
+  birthdate DATE NOT NULL,
+  phone VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  postalcode VARCHAR(255) NOT NULL,
+  streetaddr VARCHAR(255) NOT NULL,
+  regdate DATE NOT NULL,
+  cname VARCHAR(255) NOT NULL,
+  PRIMARY KEY (hinsurnum),
+  FOREIGN KEY (cname) REFERENCES priority
+);
+
+CREATE TABLE vacclocation
+(
+  lname VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  postalcode VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  PRIMARY KEY (lname)
+);
+
+CREATE TABLE hospital
+(
+  lname VARCHAR(255) NOT NULL,
+  PRIMARY KEY (lname),
+  FOREIGN KEY (lname) references vacclocation
+);
+
+
+CREATE TABLE nurse
+(
+  cnlnumber INTEGER NOT NULL,
+  nname VARCHAR(255) NOT NULL,
+  employer VARCHAR(255) NOT NULL,
+  PRIMARY KEY (cnlnumber),
+  FOREIGN KEY (employer) REFERENCES hospital
+);
+
+
+CREATE TABLE vaccdates
+(
+  lname VARCHAR(255) NOT NULL,
+  vdate DATE NOT NULL,
+  PRIMARY KEY(lname, vdate),
+  FOREIGN KEY (lname) references vacclocation
+);
+
+CREATE TABLE nurseassignments
+(
+	cnlnumber INTEGER NOT NULL,
+	lname VARCHAR(255) NOT NULL,
+	vdate DATE NOT NULL,
+	FOREIGN KEY(cnlnumber) REFERENCES nurse,
+	FOREIGN KEY(lname,vdate) REFERENCES vaccdates
+);
+
+
 CREATE TABLE batch
 (
-  batchnumber VARCHAR(30) NOT NULL,
-  mfgdate VARCHAR(30) NOT NULL,
-  expdate VARCHAR(30) NOT NULL,
-  numdoses VARCHAR(30) NOT NULL,
-  vname VARCHAR(30) NOT NULL,
-  lname VARCHAR(30) NOT NULL,
-  PRIMARY KEY (batchnumber, vname),
+  vname VARCHAR(255) NOT NULL,
+  batchnumber INTEGER NOT NULL,
+  manudate DATE NOT NULL,
+  expiradate DATE NOT NULL,
+  numvials INTEGER NOT NULL,
+  lname VARCHAR(255) NOT NULL,
+  PRIMARY KEY (vname, batchnumber),
   FOREIGN KEY (vname) REFERENCES vaccine,
   FOREIGN KEY (lname) REFERENCES vacclocation
 );
 
 CREATE TABLE vial
 (
-  vid INTEGER NOT NULL,
-  batchnumber VARCHAR(30) NOT NULL,
-  vname VARCHAR(30) NOT NULL,
-  PRIMARY KEY (vid, batchnumber, vname),
+  vname VARCHAR(255) NOT NULL,
+  batchnumber INTEGER NOT NULL,
+  vialnumber INTEGER NOT NULL,
+  PRIMARY KEY (vname, batchnumber, vialnumber),
   FOREIGN KEY (vname, batchnumber) REFERENCES batch
 );
 
@@ -111,15 +121,15 @@ CREATE TABLE slot
   slotnum INTEGER NOT NULL,
   vtime TIME NOT NULL,
   vdate DATE NOT NULL,
-  lname VARCHAR(30) NOT NULL,
-  cnlnumber INTEGER NOT NULL,
-  vid INTEGER NOT NULL,
-  batchnumber VARCHAR(30) NOT NULL,
-  vname VARCHAR(30) NOT NULL,
-  hinsurnum INTEGER NOT NULL,
+  lname VARCHAR(255) NOT NULL,
+  cnlnumber INTEGER,
+  vialnumber INTEGER,
+  batchnumber INTEGER,
+  vname VARCHAR(255),
+  hinsurnum INTEGER,
   PRIMARY KEY (slotnum, vdate, lname),
   FOREIGN KEY (lname, vdate) REFERENCES vaccdates,
   FOREIGN KEY (cnlnumber) REFERENCES nurse,
   FOREIGN KEY (hinsurnum) REFERENCES resident,
-  FOREIGN KEY (vid, batchnumber, vname) REFERENCES vial
+  FOREIGN KEY (vname, batchnumber, vialnumber) REFERENCES vial
 );
